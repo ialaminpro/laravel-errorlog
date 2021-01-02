4205d777c3af58dbc9e3129bafdd6feeacb2ed27
edorpon-nayeem <76273994+edorpon-nayeem@users.noreply.github.com>
2021-01-02 14:42:16 +0600

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,12 +1,24 @@
 <?php
+use Acolyte\ErrorLog\Http\Controllers\ErrorLogController;
+
 
 Route::prefix(config('errorlog.ROUTE_PREFIX'))->group(function () {
     Route::middleware(config('errorlog.MIDDLEWARE')?config('errorlog.MIDDLEWARE'):'web')->group(function () {
         Route::namespace('Acolyte\ErrorLog')->group(function () {
-            Route::get('error-logs', 'ErrorLogController@index');
-            Route::get('error-logs/delete/{id}', 'ErrorLogController@errorLogDelete');
-            Route::post('error-logs/delete_all', 'ErrorLogController@errorLogDeleteAll');
-            Route::get('error-logs/toggle-resolve/{id}', 'ErrorLogController@errorLogResolve');
+            $laravel = app();
+            $laravel_version = ($laravel::VERSION)[0];
+            
+            if($laravel_version <= 7) {
+                Route::get('error-logs', 'ErrorLogController@index');
+                Route::get('error-logs/delete/{id}', 'ErrorLogController@errorLogDelete');
+                Route::post('error-logs/delete_all', 'ErrorLogController@errorLogDeleteAll');
+                Route::get('error-logs/toggle-resolve/{id}', 'ErrorLogController@errorLogResolve');
+            } else {
+                Route::get('error-logs', [ErrorLogController::class, 'index']);
+                Route::get('error-logs/delete/{id}', [ErrorLogController::class, 'errorLogDelete']);
+                Route::post('error-logs/delete_all', [ErrorLogController::class, 'errorLogDeleteAll']);
+                Route::get('error-logs/toggle-resolve/{id}', [ErrorLogController::class, 'errorLogResolve']);
+            }
         });
     });
 });
